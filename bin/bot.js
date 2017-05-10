b38873dfa7f7d6b167707a2d20a64fabc27619f0
namek <ntjoel19@gmail.com>
2017-05-10 12:35:30 +0100

    siuisdf

--- bin/bot.js
+++ bin/bot.js
@@ -57,7 +57,7 @@ app.post('/buttons-actions', urlencodedParser, (req, res) =>{
         var end_pos = forwardedMSG.indexOf("|",start_pos);
         var original_channel = forwardedMSG.substring(start_pos,end_pos);
         //console.log("attachments V payload = "+JSON.stringify(actionJSONPayload.original_message.attachments)+"\n new ts="+actionJSONPayload.original_message.attachments[0].callback_id);
-        var params ={ts: actionJSONPayload.original_message.attachments[0].callback_id, channel: original_channel, as_user: true}
+        var params ={ts: actionJSONPayload.original_message.attachments[0].callback_id, channel: original_channel}
         slackbot.deleteMessage(params);
     }
     var message = {

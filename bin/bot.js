9c72244f24fd77dd0f036b8edcd29efeeeeeb24d
namek <ntjoel19@gmail.com>
2017-05-17 16:43:33 +0100

    sd

--- bin/bot.js
+++ bin/bot.js
@@ -16,17 +16,17 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 var token = process.env.bot_id_token;
 var my_token = process.env.me_id_token;
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     console.log("you just clicked a button")
     res.send("you just clicked a button")
 })
 
-app.get('/buttons-actions', (req, res) =>{
+app.get('/buttons-actions', (req, res) => {
     console.log("you just clicked a button")
     res.send("you just clicked a button")
 })
 
-function sendMessageToSlackResponseURL(responseURL, JSONmessage){
+function sendMessageToSlackResponseURL(responseURL, JSONmessage) {
     var postOptions = {
         uri: responseURL,
         method: 'POST',
@@ -36,36 +36,37 @@ function sendMessageToSlackResponseURL(responseURL, JSONmessage){
         json: JSONmessage
     }
     request(postOptions, (error, response, body) => {
-        if (error){
+        if (error) {
             // handle errors as you see fit
-            console.log("error "+error)
+            console.log("error " + error)
         }
     })
 }
 
-app.post('/buttons-actions', urlencodedParser, (req, res) =>{
+app.post('/buttons-actions', urlencodedParser, (req, res) => {
     console.log("you just clicked a button")
     res.status(200).end() // best practice to respond with 200 status
     var actionJSONPayload = JSON.parse(req.body.payload) // parse URL-encoded payload JSON string
     slackbot.actionJSONPayLoad = actionJSONPayload;
 
-    if(actionJSONPayload.actions[0].value === "dont_allow"){
+    if (actionJSONPayload.actions[0].value === "dont_allow") {
         slackbot.actionJSONPayLoad = actionJSONPayload;
         var forwardedMSG = actionJSONPayload.original_message.text;
         var start_pos = forwardedMSG.indexOf("<") + 2;
-        var end_pos = forwardedMSG.indexOf("|",start_pos);
-        var original_channel = forwardedMSG.substring(start_pos,end_pos);
+        var end_pos = forwardedMSG.indexOf("|", start_pos);
+        var original_channel = forwardedMSG.substring(start_pos, end_pos);
         //console.log("actionJSONPayLoad = "+JSON.stringify(actionJSONPayload));
         //console.log("attachments V payload = "+JSON.stringify(actionJSONPayload.original_message.attachments)+"\n new ts="+actionJSONPayload.original_message.attachments[0].callback_id);
-        var params ={token: my_token, ts: actionJSONPayload.original_message.attachments[0].callback_id, channel: original_channel}
-        var params2 ={token: my_token, ts: actionJSONPayload.message_ts, channel: actionJSONPayload.channel.id}
+        var params = { token: my_token, ts: actionJSONPayload.original_message.attachments[0].callback_id, channel: original_channel }
+        var params2 = { token: my_token, ts: actionJSONPayload.message_ts, channel: actionJSONPayload.channel.id }
         slackbot.deleteMessage(params);
         slackbot.deleteMessage(params2);
 
-    }
-    var message = {
-        "text": actionJSONPayload.user.name+" clicked: "+actionJSONPayload.actions[0].value,
-        "replace_original": false
+    } else {
+        var message = {
+            "text": forwardedMSG,
+            "replace_original": true
+        }
     }
     sendMessageToSlackResponseURL(actionJSONPayload.response_url, message)
 })
@@ -79,15 +80,15 @@ var slackbot = new SlackBot({
 });
 
 var port = process.env.PORT || 8080;
-app.listen(port, function () {
-    console.log('the server for interactive message in listening on port '+port+'!');
+app.listen(port, function() {
+    console.log('the server for interactive message in listening on port ' + port + '!');
     /**
-    * Environment variables used to configure the bot:
-    *
-    *  BOT_API_KEY : the authentication token to allow the bot to connect to your slack organization. You can get your
-    *      token at the following url: https://<yourorganization>.slack.com/services/new/bot (Mandatory)
-    *  BOT_DB_PATH: the path of the SQLite database used by the bot
-    *  BOT_NAME: the username you want to give to the bot within your organisation.
-    */
+     * Environment variables used to configure the bot:
+     *
+     *  BOT_API_KEY : the authentication token to allow the bot to connect to your slack organization. You can get your
+     *      token at the following url: https://<yourorganization>.slack.com/services/new/bot (Mandatory)
+     *  BOT_DB_PATH: the path of the SQLite database used by the bot
+     *  BOT_NAME: the username you want to give to the bot within your organisation.
+     */
     slackbot.run();
 })
